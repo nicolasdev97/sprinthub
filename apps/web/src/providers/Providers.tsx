ad8781f649cb6c16b03e5a6fae6663e848380e79
nicolasdev97 <nicolas.po.im@gmail.com>
2026-07-23 19:44:41 -0500

    feat(web): configure tanstack query provider

--- apps/web/src/providers/Providers.tsx
+++ apps/web/src/providers/Providers.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { ReactNode } from 'react';
+import { QueryClientProvider } from '@tanstack/react-query';
+
+import { queryClient } from '@/lib/react-query/queryClient';
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 export function Providers({ children }: ProvidersProps) {
-  return <>{children}</>;
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
