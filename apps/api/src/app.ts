ab3e005d838287b4851983dc7cd987a194ac72a6
nicolasdev97 <nicolas.po.im@gmail.com>
2026-07-24 20:07:09 -0500

    feat(api): configure shared logging infrastructure

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 
 import { errorHandler } from "./shared/errors";
+import { requestLogger } from "./shared/logger";
 
 const app = express();
 
 // --------------- Global middlewares ---------------
+
+app.use(requestLogger);
 
 app.use(express.json());
 
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app";
 
+import { logger } from "./shared/logger";
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
