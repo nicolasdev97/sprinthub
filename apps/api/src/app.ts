6d2079b8afae4b126b46074142b85a44b4bfb55a
nicolasdev97 <nicolas.po.im@gmail.com>
2026-07-24 21:39:25 -0500

    refactor(api): configure global application router

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import { errorHandler } from "./shared/errors";
 import { requestLogger } from "./shared/logger";
+import router from "./routes";
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(requestLogger);
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
 
 app.use(errorHandler);
 
