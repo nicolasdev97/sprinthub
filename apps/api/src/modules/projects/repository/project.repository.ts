6457e8294ca1b80059135986317bb756dadfb9b6
nicolasdev97 <nicolas.po.im@gmail.com>
2026-09-02 11:15:21 -0500

    feat(projects): implement project repository

---
+++ apps/api/src/modules/projects/repository/project.repository.ts
@@ -0,0 +1,43 @@
+import { prisma } from "../../../database/prisma";
+import { CreateProjectDto, UpdateProjectDto } from "../dto";
+
+export class ProjectRepository {
+  async findMany(workspaceId: string) {
+    return prisma.project.findMany({
+      where: {
+        workspaceId,
+      },
+    });
+  }
+
+  async findById(id: string) {
+    return prisma.project.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async create(data: CreateProjectDto) {
+    return prisma.project.create({
+      data,
+    });
+  }
+
+  async update(id: string, data: UpdateProjectDto) {
+    return prisma.project.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    return prisma.project.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
